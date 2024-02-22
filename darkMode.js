74e7aee0c572669b7431aaef4705eac69fbf6634
Rhodric McCurdy <rmccurdy268@gmail.com>
2024-02-22 12:10:17 -0700

    started implementing dark mode need to fix localstorage setting

---
+++ darkMode.js
@@ -0,0 +1,14 @@
+function darkModeCheck(){
+    const prefersDarkMode = window.matchMedia('prefers-color-scheme: dark').matches;
+    localStorage.setItem("darkMode", prefersDarkMode.valueOf);
+}
+
+function darkMode(){
+    let element = document.body;
+    element.className = "dark-mode";
+}
+
+function lightMode(){
+    let element = document.body;
+    element.className = "light-mode";
+}
