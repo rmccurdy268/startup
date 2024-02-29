96943d04e50859dbfe883364a58c853b67a6e2b0
Rhodric McCurdy <rmccurdy268@gmail.com>
2024-02-28 18:07:12 -0700

    chat is working from our side, need to set up automated responses

--- chat.js
+++ chat.js
@@ -1,12 +1,32 @@
+const allMessages = document.querySelector('.messages');
 const rhodricMessages = document.querySelector('.my-messages');
 const jarrettMessages = document.querySelector('.jarrett-mesasges');
 const chatInputForm = document.querySelector('.form-for-input');
 const chatInput = document.querySelector('.chat-input');
 
+let defaultSender = 'Rhodric'
+const setSender = (name) =>{
+    defaultSender = name;
+}
+
 const sendMessage = (e)=>{
     e.preventDefault();
     const message = {
-        sender:'',
+        sender: defaultSender,
         text: chatInput.value,
     }
+    allMessages.innerHTML += chatMessage(message);
+    chatInputForm.reset();
 }
+
+chatInputForm.addEventListener('submit', sendMessage);
+
+const chatMessage = (message) => `
+<div class="message ${message.sender === 'Rhodric' ? 'my-messages' : 'jarrett-messages'}">
+    <div class="message-sender">${message.sender}</div>
+    <div class="dm-message-text">${message.text}</div>
+</div>
+`
+
+
+
