c8051d171453d3be451e108f4b011fe31db3c424
Rhodric McCurdy <rmccurdy268@gmail.com>
2024-04-09 00:31:24 -0600

    endpoints are being called byt are not working correctly and making it to database

--- public/login.js
+++ public/login.js
@@ -1,5 +1,31 @@
 function login() {
-    const nameEl = document.querySelector("#username");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "home.html";
+    process('/auth/login');
   }
+
+function register(){
+  process('/auth/create');
+}
+
+async function process(endpoint){
+  const nameEl = document.querySelector("#username")?.value;
+  const passwordEl = document.querySelector("#password")?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: nameEl, password: passwordEl }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  if (response.ok) {
+    localStorage.setItem('userName', userName);
+    window.location.href = 'home.html';
+  } else {
+    localStorage.setItem("userName", nameEl.value);
+    window.location.href = "inspiration.html";
+    const body = await response.json();
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
+}
